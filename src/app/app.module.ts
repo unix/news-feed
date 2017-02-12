c60c13c7e580cda8c2b5fa60ab489bcd85357bd2
WittBulter <nanazuimeng123@gmail.com>
2017-02-12 22:17:31 +0800

    添加路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {FormsModule} from '@angular/forms'
+import {HttpModule} from '@angular/http'
+import {RouterModule} from '@angular/router'
+import {AppRoutingModule} from './app.routing'
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component'
+import {LoginComponent} from './login/login.component'
+import {MainComponent} from './main/main.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        MainComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by WittBulter on 2017/2/12.
+ */
+
+
+import {NgModule} from '@angular/core'
+import {Routes, RouterModule} from '@angular/router'
+
+import {LoginComponent} from './login/login.component'
+import {MainComponent} from './main/main.component'
+
+export const appRoutes: Routes = [
+    {path: '', component: LoginComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'main', component: MainComponent}
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
