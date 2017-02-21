a7198e3d7378e3d02f1e46fc1f7d7e5e6399d83e
WittBulter <nanazuimeng123@gmail.com>
2017-02-21 18:42:07 +0800

    菜单样式

--- src/app/main/main-menu/main-menu.component.ts
+++ src/app/main/main-menu/main-menu.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {MenuService} from './menu.service'
+
 
 @Component({
-  selector: 'app-main-menu',
-  templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.scss']
+    selector: 'app-main-menu',
+    templateUrl: './main-menu.component.html',
+    styleUrls: ['./main-menu.component.scss'],
+    providers: [MenuService]
 })
 export class MainMenuComponent implements OnInit {
 
-  constructor() { }
+    constructor (){
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit (){
+    }
 
 }
