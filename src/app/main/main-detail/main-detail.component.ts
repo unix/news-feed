971d37d7a3eec2dabbd1a4a0f3bab5c601adb173
WittBulter <nanazuimeng123@gmail.com>
2017-02-21 23:22:04 +0800

    详情展示

--- src/app/main/main-detail/main-detail.component.ts
+++ src/app/main/main-detail/main-detail.component.ts
@@ -1,23 +1,34 @@
 import {Component, OnInit} from '@angular/core'
 import {Router, ActivatedRoute, Params} from '@angular/router'
+import {DetailService} from './detail.service'
 
 @Component({
     selector: 'app-main-detail',
     templateUrl: './main-detail.component.html',
-    styleUrls: ['./main-detail.component.scss']
+    styleUrls: ['./main-detail.component.scss'],
+    providers: [DetailService]
 })
 export class MainDetailComponent implements OnInit {
 
-    constructor (
-        private route: ActivatedRoute,
-        private router: Router,
-    ){
+    constructor (private route: ActivatedRoute,
+                 private router: Router,
+                 private detailService: DetailService){
+    }
+    public article:any
+
+    getDetail (id: string){
+        this.detailService.getDetail(id)
+            .subscribe(
+                res =>{
+                    this.article = res
+                }
+            )
     }
 
     ngOnInit (){
-        this.route.params.forEach((params: Params) => {
+        this.route.params.forEach((params: Params) =>{
             const id = params['id']
-            if (id) console.log(id)
+            if (id) this.getDetail(id)
         })
     }
 
